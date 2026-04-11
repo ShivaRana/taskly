8d15d05e8a666d088c09e22735c2219961d905d0
Shiva Rana <shivaranaaslami@gmail.com>
2026-04-11 20:46:59 +1000

    adds functionalities

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 let taskLists = [];
 
+const hoursPerWeek = 24 * 7;
+
 const handleOnSubmit = (e) => {
-    // accessing FormData() 
-    const newForm = new FormData(e);
+  // accessing FormData()
+  const newForm = new FormData(e);
 
-    // getting data from FormData() 
-    const task = newForm.get("task");
-    const hr = newForm.get("hr");
-    const taskObj = {
-        task,
-        hr,
-        id: randomIdGenerator(),
-        type: "entry"
-    }
+  // getting data from FormData()
+  const task = newForm.get('task');
+  const hr = +newForm.get('hr'); //+ converts from string to number
+  const taskObj = {
+    task,
+    hr,
+    id: randomIdGenerator(),
+    type: 'entry',
+  };
 
-    taskLists.push(taskObj);
-    displayEntryList();
-}
+  //check for available hours to be added
+  const currentTotalHrs = taskTotal();
+  if (currentTotalHrs + hr > hoursPerWeek) {
+    return alert(
+      "Sorry, you can't add this task because it exceeds the maximum limit which is 168hrs.",
+    );
+  }
+
+  taskLists.push(taskObj);
+  displayEntryList();
+};
 
 const displayEntryList = () => {
-    let str = "";
-    // console.log(taskLists);
-    const entryElm = document.getElementById("entryList");
+  let str = '';
+  // console.log(taskLists);
+  const entryElm = document.getElementById('entryList');
 
-    const entryList = taskLists.filter((list) => list.type === "entry");
-    entryList.map((list, i) => {
-        str += `<tr>
+  const entryList = taskLists.filter((list) => list.type === 'entry');
+  entryList.map((list, i) => {
+    str += `<tr>
             <td>${i + 1}</td>
             <td>${list.task}</td>
             <td>${list.hr}</td>
@@ -34,19 +44,20 @@ const displayEntryList = () => {
                 <button onclick="switchTask('${list.id}', 'bad')" class="btn btn-success"><i class="fa-solid fa-arrow-right"></i></button>
             </td>
         </tr>`;
-    })
+  });
 
-    entryElm.innerHTML = str;
+  entryElm.innerHTML = str;
+  taskTotal();
 };
 
 const displayBadList = () => {
-    let str = "";
-    console.log(taskLists);
-    const badElm = document.getElementById("badList");
+  let str = '';
+  // console.log(taskLists);
+  const badElm = document.getElementById('badList');
 
-    const badList = taskLists.filter((list) => list.type === "bad");
-    badList.map((list, i) => {
-        str += `<tr>
+  const badList = taskLists.filter((list) => list.type === 'bad');
+  badList.map((list, i) => {
+    str += `<tr>
             <td>${i + 1}</td>
             <td>${list.task}</td>
             <td>${list.hr}</td>
@@ -55,37 +66,48 @@ const displayBadList = () => {
             <button onclick="handleOnDelete('${list.id}')" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
             </td>
         </tr>`;
-    })
+  });
 
-    badElm.innerHTML = str;
+  badElm.innerHTML = str;
+  document.getElementById('savedHrs').innerText = badList.reduce((acc, list) => acc + list.hr, 0);
 };
 
 const randomIdGenerator = (length = 6) => {
-    const str = "xuyfWpou985321sFCvhmbjdGwuqomczPa8561093wgrQpmnbgdxcuYshja856043";
-    let id = "";
+  const str =
+    'xuyfWpou985321sFCvhmbjdGwuqomczPa8561093wgrQpmnbgdxcuYshja856043';
+  let id = '';
 
-    for (let i = 0; i < 6; i++) {
-        const randomIndex = Math.floor(Math.random() * str.length);
-        id += str[randomIndex];
-    }
-    return id;
-}
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * str.length);
+    id += str[randomIndex];
+  }
+  return id;
+};
 
 const handleOnDelete = (id) => {
-    if (window.confirm("Do you want to delete this list?")) {
-        taskLists = taskLists.filter((list) => list.id !== id);
-        displayEntryList();
-        displayBadList();
-    }
-}
-
-const switchTask = (id, type) => {
-    taskLists = taskLists.map((list, i) => {
-        if (list.id === id) {
-            list.type = type
-        }
-        return list;
-    })
+  if (window.confirm('Do you want to delete this list?')) {
+    taskLists = taskLists.filter((list) => list.id !== id);
     displayEntryList();
     displayBadList();
+  }
+};
+
+const switchTask = (id, type) => {
+  taskLists = taskLists.map((list, i) => {
+    if (list.id === id) {
+      list.type = type;
+    }
+    return list;
+  });
+  displayEntryList();
+  displayBadList();
+};
+
+const taskTotal = () => {
+  const totalHrs = taskLists.reduce((acc, list) => {
+    return acc + list.hr;
+  }, 0);
+
+  document.getElementById('totalHrs').innerText = totalHrs;
+  return totalHrs;
 };
